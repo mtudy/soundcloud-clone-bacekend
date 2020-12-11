e5ebf5d6bac38c02ac76447460f80d5972139574
Surtr <cherryred5959@gmail.com>
2020-12-11 20:46:25 +0900

    remove no args constructor

--- src/main/java/io/mtudy/soundcloud/music/application/requests/CreateMusicRequest.java
+++ src/main/java/io/mtudy/soundcloud/music/application/requests/CreateMusicRequest.java
@@ -2,11 +2,9 @@ package io.mtudy.soundcloud.music.application.requests;
 
 import lombok.NoArgsConstructor;
 
+@NoArgsConstructor
 public class CreateMusicRequest {
     private String title;
-
-    public CreateMusicRequest() {
-    }
 
     public CreateMusicRequest(String title) {
         this.title = title;
