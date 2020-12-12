fca6728cc62704fd3706cc4d9fc700565497c1c6
Surtr <cherryred5959@gmail.com>
2020-12-12 23:55:50 +0900

    fix test

--- src/main/java/io/mtudy/soundcloud/SoundcloudApplication.java
+++ src/main/java/io/mtudy/soundcloud/SoundcloudApplication.java
@@ -1,9 +1,12 @@
 package io.mtudy.soundcloud;
 
+import graphql.kickstart.spring.web.boot.GraphQLWebsocketAutoConfiguration;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@SpringBootApplication
+@SpringBootApplication(exclude = {
+    GraphQLWebsocketAutoConfiguration.class
+})
 public class SoundcloudApplication {
 
     public static void main(String[] args) {
