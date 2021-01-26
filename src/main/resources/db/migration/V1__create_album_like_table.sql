fc4ede9d8fa3b78bd7699afc21857d4cf69e88c8
jeongmyeong <kimbym23@gmail.com>
2021-01-26 13:21:27 +0900

    add migration file

--- src/main/resources/db/migration/V1__create_album_like_table.sql
+++ src/main/resources/db/migration/V1__create_album_like_table.sql
@@ -1,7 +1,6 @@
-create table `like` (
+create table album_like (
     id varchar(36) not null,
     authorId varchar(36) not null,
-    type varchar(36) not null,
-    typeId varchar(36) not null,
+    albumId varchar(36) not null,
     primary key (id)
 ) engine=InnoDB collate=utf8mb4_general_ci
