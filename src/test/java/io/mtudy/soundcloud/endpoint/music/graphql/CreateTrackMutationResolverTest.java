253a740e176cede7fcd553a3ea60f14ed2f14d73
Surtr <cherryred5959@gmail.com>
2021-04-06 23:47:16 +0900

    Smoke 테스트 예제 추가 및 라이브러리 버전 업데이트 Ref #2

--- src/test/java/io/mtudy/soundcloud/endpoint/music/graphql/CreateTrackMutationResolverTest.java
+++ src/test/java/io/mtudy/soundcloud/endpoint/music/graphql/CreateTrackMutationResolverTest.java
@@ -1,36 +1,36 @@
 package io.mtudy.soundcloud.endpoint.music.graphql;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
-import com.fasterxml.jackson.databind.node.ObjectNode;
 import com.graphql.spring.boot.test.GraphQLResponse;
-import com.graphql.spring.boot.test.GraphQLTest;
 import com.graphql.spring.boot.test.GraphQLTestTemplate;
 import io.mtudy.soundcloud.WithFaker;
-import io.mtudy.soundcloud.music.application.inputs.CreateTrackInput;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+
+import java.io.IOException;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
-@GraphQLTest
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
 public class CreateTrackMutationResolverTest implements WithFaker {
-    @Autowired
-    private ObjectMapper mapper;
-
     @Autowired
     GraphQLTestTemplate graphQLTestTemplate;
 
     @Test
-    public void it_will_return_new_track_entity() throws Exception {
-        var payload = mapper.convertValue(
-            new CreateTrackInput(faker.music().chord(), faker.avatar().image()),
-            ObjectNode.class
-        );
+    public void it_will_return_new_track_entity() throws IOException {
+        var variables = new ObjectMapper().createObjectNode();
 
-        GraphQLResponse response = graphQLTestTemplate.postForResource(
-            "graphql/music/create-track.graphqls"
-        );
+        var input = new ObjectMapper().createObjectNode();
+        input.put("title", faker.music().chord());
+        input.put("artworkUrl", faker.avatar().image());
 
+        variables.set("input", input);
+
+        GraphQLResponse response = graphQLTestTemplate.perform(
+            "graphql/music/create-track.graphql",
+            variables
+        );
         assertThat(response.getStatusCode().value() == 200).isTrue();
     }
 }
